05a3855e26924b5751d952a6d9aa55208e771315
chandima-d <chandimad@amphora-research.com>
2025-08-05 19:26:40 +0100

    minor changes

--- src/utils/generated-content-dates.js
+++ src/utils/generated-content-dates.js
@@ -1,11 +1,11 @@
 // Auto-generated content dates
 export const contentDates = {
   "/blog/an-experiment/": "2025-07-16",
-  "/blog/badges/": "2025-08-04",
-  "/blog/bulls-and-better-conversations/": "2025-08-04",
+  "/blog/badges/": "2025-08-05",
+  "/blog/bulls-and-better-conversations/": "2025-08-05",
   "/blog/course-not-a-course/": "2025-07-16",
   "/blog/experiment-writeup/": "2025-07-16",
-  "/blog/modelling-sales/": "2025-07-22",
+  "/blog/modelling-sales/": "2025-08-05",
   "/blog/simon-podcast/": "2025-07-16",
   "/whitepapers/building-psychological-safety/": "2025-08-04",
   "/whitepapers/building-psychological-safety.pdf/": "2025-08-04",
